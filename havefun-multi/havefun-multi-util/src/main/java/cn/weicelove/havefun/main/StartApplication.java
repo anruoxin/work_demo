765cc9d326af69b5fe65f92dc12fb81ab7a0379d
anruoxin <675718061@qq.com>
2019-09-17 11:42:06 +0800

    初始化

---
+++ havefun-multi/havefun-multi-util/src/main/java/cn/weicelove/havefun/main/StartApplication.java
@@ -0,0 +1,12 @@
+package cn.weicelove.havefun.main;
+
+/**
+ * @author QIU PANWEI Create in 2019/9/17 11:00
+ */
+public class StartApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
